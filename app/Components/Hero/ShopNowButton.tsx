0e3035fadcc4e59169af8d5b75411a6c2c7db905
Manas Taneja <manastanejaa@gmail.com>
2024-05-25 14:04:05 +0530

    final

--- app/Components/Hero/ShopNowButton.tsx
+++ app/Components/Hero/ShopNowButton.tsx
@@ -1,15 +1,49 @@
-"use client";
-import React, { useState, useRef } from "react";
+// "use client";
+// import React, { useState, useRef } from "react";
+// import { Link, animateScroll as scroll } from "react-scroll";
+// import styles from "./Hero.module.css";
+
+// const ShopNowButton = ({}) => {
+//   return (
+//     <Link
+//       className={styles.ShopNowButton}
+//       to="Lower"
+//       smooth={true}
+//       duration={750}
+//     >
+//       Shop Now
+//     </Link>
+//   );
+// };
+
+// export default ShopNowButton;
+
+
+'use client';
+import React, { useEffect, useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import styles from "./Hero.module.css";
 
-const ShopNowButton = ({}) => {
+const ShopNowButton = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    console.log('isDisabled:', isDisabled); // Log the state after update
+  }, [isDisabled]); // Run effect when isDisabled changes
+
+  const handleClick = () => {
+    setIsDisabled(true); };
+
   return (
     <Link
-      className={styles.ShopNowButton}
-      to="Lower"
+      className={`${styles.ShopNowButton} ${isDisabled ? styles.disabled : ''}`} // Apply styles from your CSS
+      aria-disabled={isDisabled} // Add aria-disabled attribute
+      tabIndex={isDisabled ? -1 : 0} // Set tabIndex to -1 when isDisabled is true
+      to="Lower" // Target section for smooth scrolling
       smooth={true}
       duration={750}
+      disabled={isDisabled} // Disable link when isDisabled is true
+      onClick={handleClick} // Call handleClick on click
     >
       Shop Now
     </Link>
