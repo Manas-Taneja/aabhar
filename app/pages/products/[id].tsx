5634f0ea0e79efa56281acce21ad28f0c25dbe13
Manas Taneja <manastanejaa@gmail.com>
2025-02-13 14:03:53 +0530

    commit 13-02

--- app/pages/products/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function ProductPage() {
-  const { query } = useRouter();
-  const productId = query.id;
-  // Fetch or display product details
-}
